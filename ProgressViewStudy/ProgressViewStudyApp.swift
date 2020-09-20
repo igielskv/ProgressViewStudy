056e6f91629a0784cf4e60ecf38f008a4b8d0d9d
Manoli <igielskv@icloud.com>
2020-09-20 12:03:00 +0200

    Initial Commit

---
+++ ProgressViewStudy/ProgressViewStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressViewStudyApp.swift
+//  ProgressViewStudy
+//
+//  Created by Manoli on 20/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct ProgressViewStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
