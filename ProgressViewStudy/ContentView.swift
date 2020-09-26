6794bac15bad6b1db6990831627ccb9f2119b171
Manoli <igielskv@icloud.com>
2020-09-26 22:45:34 +0200

    Adding shadowColor parameter, if provided, RingProgressView drops shadow of specified color.

--- ProgressViewStudy/ContentView.swift
+++ ProgressViewStudy/ContentView.swift
@@ -10,9 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var progress = 0.6
     
-    let gradient = Gradient(colors: [.blue, .purple])
-    
-    let linearGradient = LinearGradient(gradient: Gradient(colors: [Color.blue, Color.purple]), startPoint: .topTrailing, endPoint: .bottomLeading)
+    let linearGradient = LinearGradient(gradient: Gradient(colors: [.blue, .purple]), startPoint: .topTrailing, endPoint: .bottomLeading)
     
     var body: some View {
         VStack(spacing: 30.0) {
@@ -22,9 +20,9 @@ struct ContentView: View {
                 Spacer()
                 
                 ProgressView(value: progress)
-                    .frame(width: 40.0, height: 40.0)
+                    .frame(width: 50.0, height: 50.0)
                     .onTapGesture(perform: makeProgress)
-                    .progressViewStyle(RingProgressViewStyle(content: linearGradient, lineWidth: 5.0))
+                    .progressViewStyle(RingProgressViewStyle(content: linearGradient, lineWidth: 5.0, shadowColor: Color.purple.opacity(0.5)))
             }
             
             ProgressView("LinearProgressViewStyle", value: progress)
--- ProgressViewStudy/RingProgressViewStyle.swift
+++ ProgressViewStudy/RingProgressViewStyle.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct RingProgressViewStyle<S>: ProgressViewStyle where S: ShapeStyle {
     let content: S
     let lineWidth: CGFloat
+    let shadowColor: Color
     
-    init(content: S, lineWidth: CGFloat = 4.0) {
+    init(content: S, lineWidth: CGFloat = 4.0, shadowColor: Color = Color.clear) {
         self.content = content
         self.lineWidth = lineWidth
+        self.shadowColor = shadowColor
     }
     
     func makeBody(configuration: Configuration) -> some View {
@@ -26,6 +28,7 @@ struct RingProgressViewStyle<S>: ProgressViewStyle where S: ShapeStyle {
                     .rotation(Angle(degrees: -90.0))
                     .trim(from: 0.0, to: CGFloat(fractionCompleted))
                     .stroke(content, style: StrokeStyle(lineWidth: lineWidth, lineCap: .round))
+                    .shadow(color: shadowColor, radius: lineWidth / 3, x: 0.0, y: lineWidth / 3)
             }
         }
     }
