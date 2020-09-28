b83ee415d40da7f94684232e96ef14288b9ed4e8
Manoli <igielskv@icloud.com>
2020-09-28 09:00:06 +0200

    Demo ContentView styling, adding comment to document RingProgressViewStyle use.

--- ProgressViewStudy/ContentView.swift
+++ ProgressViewStudy/ContentView.swift
@@ -15,23 +15,54 @@ struct ContentView: View {
     var body: some View {
         VStack(spacing: 30.0) {
             HStack {
-                Text("RingProgressViewStyle")
+                Text("Progress View Study")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            
+            HStack {
+                Text("My RingProgressViewStyle")
                 
                 Spacer()
+                
+                /*
+                 
+                 RingProgressViewStyle
+                 
+                 This is custom ProgressViewStyle which substitues Apple's CircularProgressViewStyle with correct behavior. Three arguments are expected where only first 'content' argument is mandatory, the other two are optional.
+                 
+                 content: has to be type of 'ShapeStyle' (Color or some Gradient would work)
+                 
+                 lineWidth: of type CGFloat obviously specifies rings line width and it's 4.0 by default
+                 
+                 shadowColor: of type Color is setting optional shadow Color, default is .clear if omitted
+                 
+                 */
                 
                 ProgressView(value: progress)
                     .frame(width: 50.0, height: 50.0)
                     .onTapGesture(perform: makeProgress)
-                    .progressViewStyle(RingProgressViewStyle(content: linearGradient, lineWidth: 5.0, shadowColor: Color.purple.opacity(0.5)))
+                    .progressViewStyle(RingProgressViewStyle(content: linearGradient, lineWidth: 8.0, shadowColor: Color.purple.opacity(0.5)))
             }
             
-            ProgressView("LinearProgressViewStyle", value: progress)
+            ProgressView("System Default LinearProgressViewStyle", value: progress)
                 .progressViewStyle(LinearProgressViewStyle())
                 .onTapGesture(perform: makeProgress)
+            
+            HStack {
+                Text("Actual 'progress' value: \(progress)")
+                Spacer()
+            }
             
             Button("Reset") {
                 progress = 0.0
             }
+            
+            Spacer()
+            
+            Text("Tap any ProgressView to \"make\" progress.")
+                .font(.footnote)
         }
         .padding()
     }
@@ -48,6 +79,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .previewLayout(.sizeThatFits)
+            
     }
 }
